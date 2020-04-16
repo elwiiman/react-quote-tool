2a713d434271ad6752c08e81ea807d7ef06c0d81
elwiiman <emmanuel.sanro@gmail.com>
2020-04-15 21:34:19 -0500

    styled component implemented in App

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
 import React from "react";
 import Header from "./components/Header";
+import styled from "@emotion/styled";
+
+const Container = styled.div`
+  max-width: 600px;
+  margin: 0 auto;
+`;
 
 function App() {
-  return <Header title={"Insurance Quote Tool"} />;
+  return (
+    <Container>
+      <Header title={"Insurance Quote Tool"} />;
+    </Container>
+  );
 }
 
 export default App;
