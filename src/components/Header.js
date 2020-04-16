a09e4bb713a645574fa6c9a71b1d2598b6a2194c
elwiiman <emmanuel.sanro@gmail.com>
2020-04-15 21:30:58 -0500

    Header component added

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,25 @@
 import React from "react";
+import styled from "@emotion/styled";
 
 const Header = ({ title }) => {
+  const HeaderContainer = styled.header`
+    background-color: #26c6da;
+    padding: 10px;
+    font-weight: bold;
+    color: #ffffff;
+  `;
+
+  const TextHeader = styled.h1`
+    font-size: 2rem;
+    margin: 0;
+    font-family: "Slabo 27px", serif;
+    text-align: center;
+  `;
+
   return (
-    <header>
-      <h1>{title}</h1>
-    </header>
+    <HeaderContainer>
+      <TextHeader>{title}</TextHeader>
+    </HeaderContainer>
   );
 };
 
