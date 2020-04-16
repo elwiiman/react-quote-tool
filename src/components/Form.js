e80902a98d7dc2873b12c48825971cb18b5ae47e
elwiiman <emmanuel.sanro@gmail.com>
2020-04-15 21:51:33 -0500

    form component completed in elements

--- src/components/Form.js
+++ src/components/Form.js
@@ -12,6 +12,29 @@ const Form = () => {
           <option value="Asian">Asian</option>
         </select>
       </div>
+      <div>
+        <label>Year</label>
+        <select>
+          <option value="">-- Select --</option>
+          <option value="2021">2021</option>
+          <option value="2020">2020</option>
+          <option value="2019">2019</option>
+          <option value="2018">2018</option>
+          <option value="2017">2017</option>
+          <option value="2016">2016</option>
+          <option value="2015">2015</option>
+          <option value="2014">2014</option>
+          <option value="2013">2013</option>
+          <option value="2012">2012</option>
+        </select>
+      </div>
+      <div>
+        <label>Plan</label>
+        <input type="radio" name="plan" value="basic" /> Basic
+        <input type="radio" name="plan" value="complete" /> Complete
+      </div>
+
+      <button>QUOTE</button>
     </form>
   );
 };
