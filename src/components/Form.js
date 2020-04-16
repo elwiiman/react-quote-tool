eeb40af0ff26fe87423a1bce6beb599c6d0ebd30
elwiiman <emmanuel.sanro@gmail.com>
2020-04-15 22:17:05 -0500

    Form component completed with styled components

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,20 +1,61 @@
 import React from "react";
+import styled from "@emotion/styled";
+
+const Field = styled.div`
+  display: flex;
+  margin-bottom: 1rem;
+  align-items: center;
+`;
+
+const Label = styled.label`
+  flex: 0 0 100px;
+`;
+
+const Select = styled.select`
+  display: block;
+  width: 100%;
+  padding: 1rem;
+  border: 1px solid #e1e1e1;
+  -webkit-appearance: none;
+`;
+
+const InputRadio = styled.input`
+  margin: 0 1rem;
+`;
+
+const Button = styled.button`
+  background-color: #00838f;
+  font-size: 16px;
+  width: 100%;
+  padding: 1rem;
+  color: #ffffff;
+  text-transform: uppercase;
+  font-weight: bold;
+  border: none;
+  margin-top: 2rem;
+
+  transition: background-color 0.3s ease;
+  &:hover {
+    cursor: pointer;
+    background-color: #26c6da;
+  }
+`;
 
 const Form = () => {
   return (
     <form>
-      <div>
-        <label>Brand</label>
-        <select>
+      <Field>
+        <Label>Brand</Label>
+        <Select>
           <option value="">-- Select --</option>
           <option value="American">American</option>
           <option value="European">European</option>
           <option value="Asian">Asian</option>
-        </select>
-      </div>
-      <div>
-        <label>Year</label>
-        <select>
+        </Select>
+      </Field>
+      <Field>
+        <Label>Year</Label>
+        <Select>
           <option value="">-- Select --</option>
           <option value="2021">2021</option>
           <option value="2020">2020</option>
@@ -26,15 +67,15 @@ const Form = () => {
           <option value="2014">2014</option>
           <option value="2013">2013</option>
           <option value="2012">2012</option>
-        </select>
-      </div>
-      <div>
-        <label>Plan</label>
-        <input type="radio" name="plan" value="basic" /> Basic
-        <input type="radio" name="plan" value="complete" /> Complete
-      </div>
+        </Select>
+      </Field>
+      <Field>
+        <Label>Plan</Label>
+        <InputRadio type="radio" name="plan" value="basic" /> Basic
+        <InputRadio type="radio" name="plan" value="complete" /> Complete
+      </Field>
 
-      <button>QUOTE</button>
+      <Button>Quote</Button>
     </form>
   );
 };
